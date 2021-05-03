05eccf21aa5040791241dfdc3180fdaaf0d444bb
DuyOnix <dtduy2210@gmail.com>
2021-05-03 15:45:16 +0700

    update react-string

--- src/BaiTapTongHop/BaiTapGameBauCua/QuanCuoc.js
+++ src/BaiTapTongHop/BaiTapGameBauCua/QuanCuoc.js
@@ -1,42 +1,45 @@
-import { useSpring } from "@react-spring/core";
-import { animated } from "@react-spring/web";
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useSpring, animated } from "react-spring";
 
 export default function QuanCuoc(props) {
   const dispatch = useDispatch();
 
+  const [propsUseSpringInCrease, setInCrease] = useSpring(() => {
+    return {
+      to: { scale: 1.25 },
+      from: { scale: 1 },
+      reset: true,
+    };
+  });
+
+  const [propsUseSpringDeCrease, setDeCrease] = useSpring(() => {
+    return {
+      to: { scale: 1.25 },
+      from: { scale: 1 },
+      reset: true,
+    };
+  });
+
   const { quanCuoc } = props;
 
-  const [propsUseSpringIncrease, setIncrease] = useSpring(() => {
-    return {
-      to: { scale: 1.25 },
-      from: { scale: 1 },
-      reset: true,
-    };
-  });
-
-  const [propsUseSpringDecrease, setDecrease] = useSpring(() => {
-    return {
-      to: { scale: 1.25 },
-      from: { scale: 1 },
-      reset: true,
-    };
-  });
-
   return (
-    <div className="mt-2">
-      <img src={quanCuoc.hinhAnh} style={{ width: 200 }} alt="bau_cua" />
+    <div className="mt-3 ">
+      <img src={quanCuoc.hinhAnh} style={{ width: 250 }} alt="" />
 
       <div
         className="bg-success mt-2 pb-2 text-center"
-        style={{ borderRadius: "10px", width: 200 }}
+        style={{ borderRadius: "10px", width: 250 }}
       >
         <animated.button
-          style={propsUseSpringIncrease}
+          style={{
+            transform: propsUseSpringInCrease.scale.interpolate(
+              (scale) => `scale(${scale})`
+            ),
+          }}
           onClick={() => {
-            setIncrease({ scale: 1 });
-            setIncrease({ scale: 1.25 });
+            setInCrease({ scale: 1 });
+            setInCrease({ scale: 1.25 });
 
             dispatch({
               type: "DAT_CUOC_BAU_CUA",
@@ -44,31 +47,34 @@ export default function QuanCuoc(props) {
               tangGiam: true,
             });
           }}
-          className="btn btn-danger mr-2"
+          className="btn btn-danger mr-3"
         >
           <i className="fa fa-plus"></i>
         </animated.button>
-        <span className="mt-2" style={{ color: "yellow", fontSize: "25px" }}>
+
+        <span className="mt-2" style={{ color: "yellow", fontSize: 25 }}>
           {quanCuoc.diemCuoc}
         </span>
+
         <animated.button
           style={{
-            transform: propsUseSpringDecrease.scale.interpolate(
+            transform: propsUseSpringDeCrease.scale.interpolate(
               (scale) => `scale(${scale})`
             ),
           }}
           onClick={() => {
-            setDecrease({ scale: 1 });
-            setDecrease({ scale: 1.25 });
+            setDeCrease({ scale: 1 });
+            setDeCrease({ scale: 1.25 });
+
             dispatch({
               type: "DAT_CUOC_BAU_CUA",
               quanCuoc,
               tangGiam: false,
             });
           }}
-          className="btn btn-danger ml-2"
+          className="btn btn-danger ml-3"
         >
-          <i class="fa fa-minus"></i>
+          -
         </animated.button>
       </div>
     </div>
